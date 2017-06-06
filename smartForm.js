7cabac3a5446b88d887fb101b6b46fb23a823082
cse101 <1420283@iub.edu.bd>
2017-06-06 10:51:25 +0600

    Assignment7

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,35 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+  name:"asd",
+  email:"fdssdf"
+};
+
+$("#name").change(function(event)
+{
+  if(("#name").val()){
+    userData.name = ("#name").val();
+    console.log("It has matched");
+  }
+});
+
+$("#startButton").click(function(event)
+{
+  $("#welcome").hide();
+  $("#q1").show();
+});
+
+$("#question2ButtonNext").click(function(event)
+{
+  if(name=="" && email=="")
+  {
+
+        $("#question2Button").hide();
+  }
+  else{
+      $("#q2").show();
+      $("#q1").hide();
+    }
+
+
+});
